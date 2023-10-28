dd4bbcad8659de89ec10c0c7b2fcba1f56a783bc
AFROJOKER <maximgav123@gmail.com>
2023-10-28 22:01:58 +0300

    Changed the name of the route to authenticion

--- app.js
+++ app.js
@@ -12,24 +12,24 @@ const app = express();
 
 //Importing the routes
 const userRoute = require('./routes/users');
-const statsRoute = require('./routes/stats');
+const authRoute = require('./routes/auth');
 
 //Running a file that connects to mongodb atlas
 require("./db/mongoConnect");
 
 //Middlewares
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: '*',
     optionsSuccessStatus: 200, // Some legacy browsers (IE11) choke on 204
 }));
 app.use(express.json());
 app.use(cookieParser());
 
-
+    
 //Connecting the routes with our app
 app.get("/",(req,res)=>{res.json("hi")})
 app.use("/users",userRoute);
-app.use("/stats",statsRoute);
+app.use("/auth",authRoute);
 
 //Getting the server's port and listening to it
 const port = process.env.PORT || 3001;
